5e9a6307b6b164ba824e29c4a6325928aadfdf0b
xMatty522 <bronczek522@gmail.com>
2023-01-20 20:30:43 +0100

    działa es

--- PYTHON/praca_domowa/zad 1.py
+++ PYTHON/praca_domowa/zad 1.py
@@ -1,7 +1,18 @@
 plik = open('C:\\Users\\Szef\\Desktop\\pliki zaliczenia\\palindromy.txt', 'r')
 dane = plik.readlines()
-print(dane)
+# print(dane)
+plik2 = open('C:\\Users\\Szef\\Desktop\\pliki zaliczenia\\palindromy_wynik.txt', 'w')
 for i in range(len(dane)):
+    dane[i] = dane[i].replace('\n','')
     dane[i] = dane[i].replace(" ","")
+    
+    if dane[i] == dane[i][::-1]:
+        plik2.write(dane[i])
+        plik2.write('\n')
+    else:
+        pass
+        
 print(dane)
+
 plik.close()
+plik2.close()
